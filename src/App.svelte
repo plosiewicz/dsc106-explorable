f4d57463317da9324eaf737552e50d05654ad024
Paul Losiewicz <plosiewi@ucsd.edu>
2024-05-29 12:24:56 -0700

    initial

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  import Meta from "./Meta.svelte";
+  import Title from "./Components/Title.svelte";
+  import Intro from "./Components/Intro.svelte";
+  import TextAndMathEquations from "./Components/TextAndMathEquations.svelte";
+  import LineChart from "./Components/LineChart.svelte";
+  import ScrollCenter from "./Components/ScrollCenter.svelte";
+  import Conclusion from "./Components/Conclusion.svelte";
+  import Resources from "./Components/Resources.svelte";
+  import ScrollSide from "./Components/ScrollSide.svelte";
+  import Heatmap from './Components/Heatmap.svelte';
+</script>
+
+<Meta />
+<Title />
+<Intro />
+<TextAndMathEquations />
+<ScrollSide />
+<Heatmap />
+<LineChart />
+<ScrollCenter />
+<Conclusion />
+<Resources />
