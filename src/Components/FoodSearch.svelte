5e5666276eb77f84a50d676bfb54bf31c40f44b6
Paul Losiewicz <plosiewi@ucsd.edu>
2024-05-30 20:25:44 -0700

    quick fix

--- src/Components/FoodSearch.svelte
+++ src/Components/FoodSearch.svelte
@@ -13,7 +13,7 @@
   const dispatch = createEventDispatcher(); // Create dispatcher
 
   onMount(async () => {
-    const response = await fetch("/dsc-explorable/data/food.csv");
+    const response = await fetch("/dsc106-explorable/data/food.csv");
     const data = await response.text();
     foodData = d3.csvParse(data, (d) => ({
       name: d.Description,
